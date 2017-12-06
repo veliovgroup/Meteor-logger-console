17a45a50709924abefefdcc55aab25f179918ba5
dr.dimitru <ceo@veliov.com>
2017-12-06 08:49:21 +0300

    v2.0.3
     - Fix #8 , thanks to @chafey
     - Dependencies update
     - Compatibility with `meteor@1.6`
     - Community meta, docs, guidelines, and etc.

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'ostrio:loggerconsole',
-  version: '2.0.2',
+  version: '2.0.3',
   summary: 'Logging: Print Client\'s logs to Server\'s console, messages colorized for better readability',
   git: 'https://github.com/VeliovGroup/Meteor-logger-console',
   documentation: 'README.md'
@@ -8,12 +8,12 @@ Package.describe({
 
 Package.onUse(function(api) {
   api.versionsFrom('1.4');
-  api.use(['ecmascript', 'check', 'underscore', 'ostrio:logger@2.0.4'], ['client', 'server']);
+  api.use(['ecmascript', 'check', 'underscore', 'ostrio:logger@2.0.5'], ['client', 'server']);
   api.mainModule('loggerconsole.js', ['client', 'server']);
 });
 
 Package.onTest(function(api) {
   api.use('tinytest');
-  api.use(['ecmascript', 'underscore', 'ostrio:logger@2.0.4', 'ostrio:loggerconsole@2.0.2']);
+  api.use(['ecmascript', 'underscore', 'ostrio:logger@2.0.5', 'ostrio:loggerconsole@2.0.3']);
   api.addFiles('loggerconsole-tests.js');
 });
